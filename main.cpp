adc461f883020c98e115967b03e6c464f5fb6c27
albkharisov1 <albkharisov@gmail.com>
2018-10-09 15:51:10 +0300

    123

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 
 #include <spdlog/spdlog.h>
 #include <stdio.h>
+#include "version.h"
 
 int main()
 {
     auto logger = spdlog::stdout_logger_mt("console");
     logger->info("version {} was started", version());
 
-    printf("build %ul\r\n", TRAVIS_BUILD_NUMBER);
+    printf("build %ul\r\n", PROJECT_VERSION_PATCH);
     printf("Hello, World!\r\n");
     return 0;
 }
