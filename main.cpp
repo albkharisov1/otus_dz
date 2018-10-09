b89f9dee32c204a8bc35cbaca267845f082a8703
albkharisov1 <albkharisov@gmail.com>
2018-10-09 13:44:41 +0300

    add printf

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "lib.h"
 
 #include <spdlog/spdlog.h>
+#include <stdio.h>
 
 int main()
 {
     auto logger = spdlog::stdout_logger_mt("console");
     logger->info("version {} was started", version());
 
+    printf("%ul\r\n", TRAVIS_BUILD_NUMBER);
+    printf("Hello World!\r\n");
     return 0;
 }
