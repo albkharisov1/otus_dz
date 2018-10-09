4e60ff0213b7058a2df3a09867304cb087713c0c
albkharisov1 <albkharisov@gmail.com>
2018-10-09 13:46:47 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
     auto logger = spdlog::stdout_logger_mt("console");
     logger->info("version {} was started", version());
 
-    printf("%ul\r\n", TRAVIS_BUILD_NUMBER);
-    printf("Hello World!\r\n");
+    printf("build %ul\r\n", TRAVIS_BUILD_NUMBER);
+    printf("Hello, World!\r\n");
     return 0;
 }
