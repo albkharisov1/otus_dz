ffea8d28edf6698af40728a929f2e68438735933
albkharisov1 <albkharisov@gmail.com>
2018-10-11 12:06:38 +0300

    DONE DONE DONE

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 
 #include <spdlog/spdlog.h>
 #include <stdio.h>
-#include "version.h"
+#include "lib.h"
 
 int main()
 {
 //    auto logger = spdlog::stdout_logger_mt("console");
 //    logger->info("version {} was started", version());
 
-    printf("build %d\r\n", PROJECT_VERSION_PATCH);
+    printf("build %d\r\n", version());
     printf("Hello, World!\r\n");
     return 0;
 }
